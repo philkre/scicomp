aa7d38f260ff2372f3e0b225d6d0f7c86c1450b7
philkre <p.kreiter@icloud.com>
2026-02-02 12:18:36 +0100

    init

---
+++ main.jl
@@ -0,0 +1,12 @@
+println("Hello, World!")
+
+a = 3
+b = 5
+
+function add(x, y)
+    return x + y
+end
+
+result = add(a, b)
+println("The sum of $a and $b is $result")
+println("This is a simple Julia program.")
