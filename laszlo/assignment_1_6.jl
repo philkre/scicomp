5fdf6709d63d80bcc08c984bc288219832b2902f
Laszlo Schoonheid <lschoonheid@gmail.com>
2026-02-22 19:11:24 +0100

    remove duplicate

--- laszlo/assignment_1_6.jl
+++ laszlo/assignment_1_6.jl
@@ -23,17 +23,6 @@ function _resolve_output_path(output::String)::String
     return normpath(joinpath(@__DIR__, output))
 end
 
-function _savefig(output::String)
-    output_path = _resolve_output_path(output)
-    mkpath(dirname(output_path))
-    if isfile(output_path)
-        rm(output_path; force=true)
-    end
-    savefig(output_path)
-    isfile(output_path) || error("Failed to save plot to $output_path")
-    return output_path
-end
-
 function _savefig(p, output::String)
     output_path = _resolve_output_path(output)
     mkpath(dirname(output_path))
